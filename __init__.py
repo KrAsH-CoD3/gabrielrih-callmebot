fac6fe1269fb4283b70436a09459a6a8c496871a
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2022-07-17 10:02:32 -0300

    Test it as package

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from callmebot import sendFreeMessage
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='callmebot',
+    version='1.0.0',
+    author='Gabriel Richter',
+    author_email='gabrielrih@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/gabrielrih/callMeBot',
+    license='MIT',
+    packages=['callmebot'],
+    install_requires=['requests, yaml'],
+)
